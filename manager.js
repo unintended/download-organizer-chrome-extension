22b4c2e8e4cbea188faf775ae59da1ea5cf70d95
Ilya Samartsev <ignz@yandex-team.ru>
2021-06-03 10:29:40 +0300

    remove old migration code

--- manager.js
+++ manager.js
@@ -121,19 +121,6 @@ chrome.downloads.onDeterminingFilename.addListener(function (downloadItem, sugge
 
 var version = localStorage.getItem('version');
 
-if (version !== null) {
-    if (version < '0.2.4') {
-        var rulesetsStr = localStorage.getItem('rulesets');
-        if (rulesetsStr) {
-            var rulesets = JSON.parse(rulesetsStr);
-            rulesets.forEach(rule => {
-                rule.enabled = true;
-            });
-            localStorage.setItem('rulesets', JSON.stringify(rulesets));
-        }
-    }
-}
-
 if (!version || version != chrome.runtime.getManifest().version) {
     // Open the options page directly after installing or updating the extension
     localStorage.setItem('version', chrome.runtime.getManifest().version);
